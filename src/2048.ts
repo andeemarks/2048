200dfa15e5e277b605f9c1a327ea47f17e7fcff8
Andy Marks <vampwillow@gmail.com>
2020-12-29 09:07:36 +1100

    + left tilt behaviour

--- src/2048.ts
+++ src/2048.ts
@@ -6,41 +6,76 @@ class Board {
     [0, 0, 0, 0],
   ];
 
-  width(): Number {
-      return this._board[0].length;
+  width(): number {
+    return this._board[0].length;
   }
 
-  height(): Number {
-      return this._board.length;
+  height(): number {
+    return this._board.length;
   }
 
   spaces(): Number[] {
-     return ([] as Number[]).concat(...this._board); 
+    return ([] as Number[]).concat(...this._board);
   }
 
   populate(column: number, row: number, value: number) {
-    this._board[column][row] = value;
+    this._board[row][column] = value;
   }
 
   toString(): String {
-    //   let result = "";
-      return this._board.join("\n");
+    let result = "";
+    for (let i = this._board.length; i > 0; i--) {
+      result = result + this._board[i - 1].join("") + "\n";
+    }
+    return result;
+  }
+
+  rowAtPosition(position: number): Number[] {
+    return this._board[position - 1];
+  }
+
+  setRow(position: number, row: Number[]) {
+    this._board[position] = row;
+  }
+
+  print() {
+    console.log(this.toString());
+  }
+
+  tiltLeftRow(row: Number[]): Number[] {
+    let populatedSpaces = row.filter((space: Number) => {return space != 0;});
+    let tiltedRow = populatedSpaces.concat([0, 0, 0, 0]).slice(0, row.length);
+
+    console.log(tiltedRow);
+
+    return tiltedRow;
+  }
+
+  tiltLeft() {
+    this.print();
+
+    let tiltedBoard = new Board();
+    for (let row = 0; row < this._board.length; row++) {
+      tiltedBoard.setRow(row, this.tiltLeftRow(this._board[row]));
+    }
+
+    return tiltedBoard;
   }
 }
 
 class TwentyFortyEight {
-    private _board = new Board();
+  private _board = new Board();
 
-    board(): Board {
-        return this._board;
-    }
+  board(): Board {
+    return this._board;
+  }
 
-    shuffle(unshuffled: number[]): number[] {
-        return unshuffled
-          .map((a) => ({ sort: Math.random(), value: a }))
-          .sort((a, b) => a.sort - b.sort)
-          .map((a) => a.value);
-    }
+  shuffle(unshuffled: number[]): number[] {
+    return unshuffled
+      .map((a) => ({ sort: Math.random(), value: a }))
+      .sort((a, b) => a.sort - b.sort)
+      .map((a) => a.value);
+  }
 
   start(): Board {
     let startRows = this.shuffle([0, 1, 2, 3]);
@@ -49,11 +84,14 @@ class TwentyFortyEight {
     this._board.populate(startColumns[0], startRows[0], 2);
     this._board.populate(startColumns[1], startRows[1], 2);
 
-    console.log(this._board.toString());
+    // console.log(this._board.toString());
 
     return this._board;
+  }
+
+  tiltLeft(): Board {
+    return this._board.tiltLeft();
   }
 }
 
 export { TwentyFortyEight, Board };
-
--- test/2048.test.ts
+++ test/2048.test.ts
@@ -1,24 +1,27 @@
-import {TwentyFortyEight, Board} from "../src/2048";
+import { TwentyFortyEight, Board } from "../src/2048";
 
-describe('2048', () => {
-
+describe("2048", () => {
   const game = new TwentyFortyEight();
 
-  it('holds a board of the right dimensions', () => {
+  it("holds a board of the right dimensions", () => {
     const board = game.board();
 
     expect(board).toBeDefined();
     expect(board.width()).toBe(4);
     expect(board.height()).toBe(4);
-  })
+  });
 
-  it('the board starts empty', () => {
+  it("the board starts empty", () => {
     const spaces = game.board().spaces();
 
-    expect(spaces.every((space: Number) => {return space == 0;})).toBeTruthy();
-  })
+    expect(
+      spaces.every((space: Number) => {
+        return space == 0;
+      })
+    ).toBeTruthy();
+  });
 
-  it('populates 2 spaces on the board when starting', () => {
+  it("populates 2 spaces on the board when starting", () => {
     const spaces = game.start().spaces();
 
     expect(
@@ -26,10 +29,9 @@ describe('2048', () => {
         return space == 2;
       }).length
     ).toBe(2);
-    
-  })
+  });
 
-  it('populates 2 different spaces each start', () => {
+  it("populates 2 different spaces each start", () => {
     const spaces1 = new TwentyFortyEight().start().spaces();
     const spaces2 = new TwentyFortyEight().start().spaces();
     const spaces3 = new TwentyFortyEight().start().spaces();
@@ -38,5 +40,31 @@ describe('2048', () => {
     expect(spaces1).not.toEqual(spaces2);
     expect(spaces2).not.toEqual(spaces3);
     expect(spaces3).not.toEqual(spaces4);
-  })
-})
+  });
+
+  it("slides unsupported numbers to the bottom when tilting down", () => {
+    let game = new TwentyFortyEight();
+    let originalBoard = game.board();
+
+    originalBoard.populate(0, 2, 2);
+    originalBoard.populate(1, 2, 2);
+    originalBoard.populate(1, 1, 2);
+    originalBoard.populate(2, 1, 2);
+    originalBoard.populate(2, 3, 2);
+    originalBoard.populate(3, 0, 2);
+    originalBoard.populate(3, 2, 2);
+
+    let tiltedBoard = game.tiltLeft();
+
+    let expectedBoard = new TwentyFortyEight().board();
+    expectedBoard.populate(0, 0, 2);
+    expectedBoard.populate(0, 1, 2);
+    expectedBoard.populate(0, 2, 2);
+    expectedBoard.populate(0, 3, 2);
+    expectedBoard.populate(1, 1, 2);
+    expectedBoard.populate(1, 2, 2);
+    expectedBoard.populate(2, 2, 2);
+
+    expect(tiltedBoard).toEqual(expectedBoard);
+  });
+});
