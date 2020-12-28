0e499df7d38fed441f9c42ab9198f6041992cb6e
Andy Marks <vampwillow@gmail.com>
2020-12-28 12:54:07 +1100

    + board population on start

--- src/2048.ts
+++ src/2048.ts
@@ -6,6 +6,12 @@ class TwentyFortyEight {
     board(): Board {
         return this._board;
     }
+
+    start(): Board {
+        this._board[0][1] = 2;
+        this._board[2][3] = 2;
+        return this._board;
+    }
 }
 
 export {TwentyFortyEight};
--- test/2048.test.ts
+++ test/2048.test.ts
@@ -6,7 +6,7 @@ describe('2048', () => {
 
   it('holds a board of the right dimensions', () => {
     const board = game.board();
-    
+
     expect(board).toBeDefined();
     expect(board.length).toBe(4);
     expect(board[0].length).toBe(4);
@@ -19,4 +19,15 @@ describe('2048', () => {
     expect(spaces.every((space: Number) => {return space == 0;})).toBeTruthy();
   })
 
+  it('populates 2 spaces on the board when starting', () => {
+    const board: Board = game.start();
+    const spaces = ([] as Number[]).concat(...board);
+
+    expect(
+      spaces.filter((space: Number) => {
+        return space == 2;
+      }).length
+    ).toBe(2);
+    
+  })
 })
