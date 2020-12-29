470899d3e399e3766fd49be2311aaeeafdfaacaf
Andy Marks <vampwillow@gmail.com>
2020-12-30 09:16:12 +1100

    Red test before implementing new square birth behaviour

--- test/board-control.test.ts
+++ test/board-control.test.ts
@@ -1,7 +1,21 @@
 import BoardControl from "../src/board-control";
+import Board from "../src/board";
 
 describe("BoardControl", () => {
-  const boardControl = new BoardControl();
+  function countPopulatedSquares(board: Board): number {
+    return board.flatten().filter((space: number) => {
+      return space > 0;
+    }).length;
+  }
 
-  it("guards attempts to access out-of-bound rows", () => {});
+  it("randomly populates a new empty square when tilting", () => {
+    let board = new Board();
+    let boardPopulationCount = countPopulatedSquares(board);
+    let newBoard = BoardControl.tiltDown(board);
+    let newBoardPopulationCount = countPopulatedSquares(newBoard);
+
+    expect(newBoardPopulationCount).toEqual(boardPopulationCount + 1);
+  });
+
+  it("errors when no remaining empty squares are available", () => {});
 });
