ae148427c0c5a16edb17d5ae6605c88310281329
Andy Marks <vampwillow@gmail.com>
2025-06-09 09:25:08 +1000

    Moved all DIsplay logic into UIRenderer

--- src/cli/ui-renderer.ts
+++ src/cli/ui-renderer.ts
@@ -3,16 +3,13 @@ import clear from "clear";
 import figlet from "figlet";
 import boxen from "boxen";
 import Board from "../board";
-import Display from "../display";
 import { EndReason } from "./game-controller";
 
 export class UIRenderer {
-  private display: Display;
   private normalBoardBoxProps: boxen.Options;
   private levelUpBoardBoxProps: boxen.Options;
 
   constructor() {
-    this.display = new Display();
     this.normalBoardBoxProps = {
       padding: 0,
       margin: 0,
@@ -39,7 +36,7 @@ export class UIRenderer {
     console.log(chalk.yellow(figlet.textSync("2048-ts", { horizontalLayout: "full" })));
     console.log(chalk.bgWhiteBright.black(`Score: ${score.toString()}`));
 
-    const formattedBoard = this.display.format(board);
+    const formattedBoard = this.format(board);
     
     if (hasNewScoreLevel) {
       console.log(boxen(formattedBoard, this.levelUpBoardBoxProps));
@@ -48,33 +45,100 @@ export class UIRenderer {
     }
   }
 
+  redLog(message: string): void {
+    console.log(
+      chalk.red(
+        figlet.textSync(message, { horizontalLayout: "full" })
+      )
+    );
+  }
+
+  greenLog(message: string): void {
+    console.log(
+      chalk.green(
+        figlet.textSync(message, { horizontalLayout: "full" })
+      )
+    );
+  }
+
   showGameEnd(reason: EndReason): void {
     console.log();
     switch (reason) {
       case EndReason.PlayerQuit:
-        console.log(
-          chalk.red(
-            figlet.textSync("Thanks for playing", { horizontalLayout: "full" })
-          )
-        );
+        this.redLog("Thanks for playing");
         break;
       case EndReason.BoardFull:
-        console.log(
-          chalk.red(
-            figlet.textSync("Game over - board full", { horizontalLayout: "full" })
-          )
-        );
+        this.redLog("Game over - board full");
         break;
       case EndReason.BoardComplete:
-        console.log(
-          chalk.green.bold(
-            figlet.textSync("Congratulations - you WON!!!", {
-              horizontalLayout: "full",
-            })
-          )
-        );
+        this.greenLog("Congratulations - you WON!!!");
         break;
     }
     process.exit(0);
   }
+  
+  pad(value: number): number | string {
+    if (value <= 9) {
+      return `  ${value.toString()} `;
+    }
+    if (value <= 99) {
+      return ` ${value.toString()} `;
+    }
+    if (value <= 999) {
+      return ` ${value.toString()}`;
+    }
+    return value;
+  }
+
+  format(board: Board): string {
+    const spaces = board.spaces();
+    let displaySpace = "";
+    for (let row = board.height() - 1; row >= 0; row--) {
+      for (let column = 0; column < board.rowAtPosition(row).length; column++) {
+        const space = spaces[row][column];
+        switch (space) {
+          case 0:
+            displaySpace += chalk.bgBlack("    ");
+            break;
+          case 2:
+            displaySpace += chalk.black.bgWhite(this.pad(space));
+            break;
+          case 4:
+            displaySpace += chalk.black.bgWhiteBright(this.pad(space));
+            break;
+          case 8:
+            displaySpace += chalk.black.bgCyanBright(this.pad(space));
+            break;
+          case 16:
+            displaySpace += chalk.black.bgCyan(this.pad(space));
+            break;
+          case 32:
+            displaySpace += chalk.black.bgBlueBright(this.pad(space));
+            break;
+          case 64:
+            displaySpace += chalk.black.bgBlue(this.pad(space));
+            break;
+          case 128:
+            displaySpace += chalk.black.bgMagentaBright(this.pad(space));
+            break;
+          case 256:
+            displaySpace += chalk.whiteBright.bgMagenta(this.pad(space));
+            break;
+          case 512:
+            displaySpace += chalk.black.bgYellow(this.pad(space));
+            break;
+          case 1024:
+            displaySpace += chalk.black.bgYellowBright(this.pad(space));
+            break;
+          case 2048:
+            displaySpace += chalk.whiteBright.bgRedBright(this.pad(space));
+            break;
+        }
+      }
+      displaySpace += "\n";
+    }
+
+    return displaySpace.trim();
+  }
+
 }
