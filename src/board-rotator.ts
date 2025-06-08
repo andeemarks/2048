b98c9eec92925530677a7f13897446d0dc683f9e
Andy Marks <vampwillow@gmail.com>
2025-06-08 13:59:01 +1000

    Used more specific RangeError where appropriate

--- src/board-rotator.ts
+++ src/board-rotator.ts
@@ -3,7 +3,7 @@ import Board from "./board";
 class BoardRotator {
   static rotate90Degrees(board: Board, rotationCount = 1): Board {
     if (rotationCount < 1 || rotationCount >= 4) {
-      throw new Error();
+      throw new RangeError();
     }
 
     let spaces = board.spaces();
